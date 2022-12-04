209fe70a72b960f128ceaab9f0b674e7cf292215
Fredrik Larsson <fredrik@wizardry.se>
2022-12-04 12:38:52 +0100

    Solved day 4

---
+++ 4_part2_solution.py
@@ -0,0 +1,17 @@
+import sys
+
+count = 0
+
+def subset(a, b):
+    return a[0] >= b[0] and a[0] <= b[1] or a[1] >= b[0] and a[1] <= b[1]
+
+def overlap(a, b):
+    return subset(a, b) or subset(b, a)
+
+for line in sys.stdin:
+    a, b = line.rstrip().split(',')
+    assignment_a, assignment_b = a.split('-'), b.split('-')
+    if overlap([int(assignment_a[0]), int(assignment_a[1])], [int(assignment_b[0]), int(assignment_b[1])]):
+        count += 1
+
+print(f"{count}")
