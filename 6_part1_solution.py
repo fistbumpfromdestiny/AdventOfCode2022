3134b616916bfdb9ed21943409682020c5cdd27a
Fredrik Larsson <fredrik@wizardry.se>
2022-12-06 09:06:08 +0100

    Solved day 6

---
+++ 6_part1_solution.py
@@ -0,0 +1,22 @@
+import sys
+
+stream = []
+counter = 0
+
+with open("input.txt") as f:
+    while True:
+        char = f.read(1)
+        if not char:
+            break
+        if len(stream) < 4:
+            stream.append(char)
+        else:
+            if(len(set(stream)) != len(stream)):
+                stream.pop(0)
+                stream.append(char)
+            else: 
+                break
+        counter += 1
+   
+print(f"{counter}")
+    
